37ed90e013d086ffba6809fe6ad901865b40c65e
hidykong <hidykong@gmail.com>
2014-07-16 17:36:09 -0500

    Major update Page2
    modified to take page2.json as the input.
    Please check it for the formatting of the posts

--- app/page2.js
+++ app/page2.js
@@ -35,58 +35,65 @@ window.onload = function(){
 	//update the postNo to pass to PHP through the hidden input
     document.getElementById("postNo").value = params.post;
 
-    //retrieve the textfile with the post information
-	var txtFile = new XMLHttpRequest();
-	var filename = "post" + params.post + ".txt";
-	txtFile.open("GET", filename, true);
-	console.log("test");
-	txtFile.onreadystatechange = function() {
-	  if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
-	    if (txtFile.status === 200) {  // Makes sure it's found the file.
-			allText = txtFile.responseText; 
-			lines = txtFile.responseText.split("\n"); // Will separate each line into an array
-			
-			//change the language mentioned in Q4 depending on language of the post
+
+    $.getJSON( "page2.json", function( data ) {
+
+    	//read the json file and save all posts in items
+  		var items = [];
+	  	$.each( data, function( key, val ) {
+	   	 items.push(val);
+	  	});
 
 
-			if (lines[0].indexOf("Korean") > -1){
-				document.getElementById("Q4").innerHTML = "4. Why did you write this post in Korean?";
-				var appropriate = $('#reasonAppropriate');
-			    appropriate[0].nextSibling.nodeValue = "Korean is more appropriate for expressing the content";
+	 currentPost = items[params.post-1];
 
-				var know = $('#reasonKnow');
-			    newText = "Didn't know the terms/appropriate expression in English";
-			    know[0].nextSibling.nodeValue = newText;
+	 currentLang = currentPost.lang;
+	 document.getElementById("date").innerHTML = currentPost.date;
+	 document.getElementById("type").innerHTML = currentPost.type;
+	 document.getElementById("message").innerHTML = currentPost.message;
+	 if (currentPost.type == "Comment"){
+	 	document.getElementById("box").style.backgroundColor = "#F6F7F8";
+	 }
+	 document.getElementById("original").href = currentPost.original;
 
-		    	var translateQ = document.getElementById("translate");
-		    	translateQ.style.visibility = 'visible';
-		    	translateQ.style.height = '80px';
-
-		    	var audienceText = document.getElementById("Q3c").innerHTML;
-		    	var newText = audienceText.replace("English", "Korean");
-		    	document.getElementById("Q3c").innerHTML = newText;
-
-		    } else if (lines[0].indexOf("Both") > -1){
-		    	document.getElementById("Q4").innerHTML = "4a. Why did you write part of this post in English?";
-		    	var bothLang = document.getElementById("bothLang");
-		    	bothLang.style.visibility = 'visible';
-		    	bothLang.style.height = '170px';
-
-		    	var translateQ = document.getElementById("translate");
-		    	translateQ.style.visibility = 'visible';
-		    	translateQ.style.height = '80px';
-
-		    	var audienceText = document.getElementById("Q3c").innerHTML;
-		    	var newText = audienceText.replace("English", "both languages");
-		    	document.getElementById("Q3c").innerHTML = newText;
-			}
-	    }
-	  }
-
-	}
-	txtFile.send(null);
+	 console.log(currentPost.date);
+    //update the post
+	//change the language mentioned in Q4 depending on language of the post
 
 
+		if (currentLang.indexOf("Korean") > -1){
+			document.getElementById("Q4").innerHTML = "4. Why did you write this post in Korean?";
+			var appropriate = $('#reasonAppropriate');
+		    appropriate[0].nextSibling.nodeValue = "Korean is more appropriate for expressing the content";
+
+			var know = $('#reasonKnow');
+		    newText = "Didn't know the terms/appropriate expression in English";
+		    know[0].nextSibling.nodeValue = newText;
+
+	    	var translateQ = document.getElementById("translate");
+	    	translateQ.style.visibility = 'visible';
+	    	translateQ.style.height = '80px';
+
+	    	var audienceText = document.getElementById("Q3c").innerHTML;
+	    	var newText = audienceText.replace("English", "Korean");
+	    	document.getElementById("Q3c").innerHTML = newText;
+
+	    } else if (currentLang.indexOf("Both") > -1){
+	    	document.getElementById("Q4").innerHTML = "4a. Why did you write part of this post in English?";
+	    	var bothLang = document.getElementById("bothLang");
+	    	bothLang.style.visibility = 'visible';
+	    	bothLang.style.height = '170px';
+
+	    	var translateQ = document.getElementById("translate");
+	    	translateQ.style.visibility = 'visible';
+	    	translateQ.style.height = '80px';
+
+	    	var audienceText = document.getElementById("Q3c").innerHTML;
+	    	var newText = audienceText.replace("English", "both languages");
+	    	document.getElementById("Q3c").innerHTML = newText;
+		}
+
+	});
 };
 
 
