22c44ce1617a560b5e3dd4e74625a5ccc71ae067
SudoAttack <wayne8798@gmail.com>
2014-08-29 17:09:07 -0500

    add mvp for page2 analyzer

---
+++ backend/page2_analyzer.py
@@ -0,0 +1,100 @@
+import sys
+
+pdata = open(sys.argv[1], "r")
+lines = pdata.readlines()
+pdata.close()
+
+output = ""
+
+flag3 = False
+dict3 = {"family":0, "work":1, "friends":2, "hobby":3, \
+	"neighbor":4, "religious":5, "everyone":6, "other":7}
+data3 = [0,0,0,0,0,0,0,0]
+
+flag4 = False
+dict4 = {"appropriate":0, "audience":1, "quote":2, "habitual":3, \
+"knowledge":4, "other":5}
+data4 = [0,0,0,0,0,0]
+
+flag4b = False
+dict4b = {"appropriate":0, "audience":1, "quote":2, "habitual":3, \
+"knowledge":4, "other":5}
+data4b = [0,0,0,0,0,0]
+for line in lines:
+	if len(line) >= 6 and line[:4] == "Post":
+		print output
+		output = line[5]
+		continue
+
+	if len(line) >= 2 and line[:2] == "1.":
+		if line[41:43] == "No":
+			output += ",0"
+		else:
+			output += ",1"
+		continue
+
+	if len(line) >= 2 and line[:2] == "2.":
+		if line[42:44] == "No":
+			output += ",0"
+		else:
+			output += ",1"
+		continue
+
+	if len(line) >= 2 and line[:2] == "3.":
+		flag3 = True
+		continue
+
+	if len(line) > 2 and flag3 == True:
+		data3[dict3[line[:-2]]] = 1
+		continue
+
+	if len(line) == 2 and flag3 == True:
+		flag3 = False
+		for i in data3:
+			output += "," + str(i)
+		data3 = [0,0,0,0,0,0,0,0]
+		continue
+
+	if len(line) >= 2 and line[:2] == "3b":
+		if line[81:83] == "No":
+			output += ",0"
+		else:
+			output += ",1"
+		continue
+
+	if len(line) >= 2 and line[:2] == "3c":
+		output += "," + line[96]
+		continue
+
+	if len(line) >= 2 and line[:2] == "4.":
+		flag4 = True
+		continue
+
+	if len(line) > 2 and flag4 == True:
+		data4[dict4[line[:-1]]] = 1
+		continue
+
+	if len(line) == 2 and flag4 == True:
+		flag4 = False
+		for i in data4:
+			output += "," + str(i)
+		data4 = [0,0,0,0,0,0]
+		continue
+
+	if len(line) >= 2 and line[:2] == "4b":
+		flag4b = True
+		continue
+
+	if len(line) > 2 and flag4b == True:
+		data4b[dict4b[line[:-1]]] = 1
+		continue
+
+	if len(line) == 2 and flag4b == True:
+		flag4b = False
+		for i in data4b:
+			output += "," + str(i)
+		data4b = [0,0,0,0,0,0]
+		continue
+
+	if len(line) >=2 and line[:2] == "5.":
+		output += "," + line[113]
